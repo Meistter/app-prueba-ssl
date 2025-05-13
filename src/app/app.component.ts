9a627b2823a7203ceff88e45f03e35d393675148
Meistter <sperez@bancomercantil.com>
2025-05-13 16:17:24 -0400

    Agrega ssl y logica de otp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import eruda from 'eruda';
+eruda.init()
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +10,45 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'app-prueba-ssl';
+
+
+  otpListener(): void{
+    if ("OTPCredential" in window) {
+      console.log('Explorador valido');
+      
+  // window.addEventListener("DOMContentLoaded", (e) => {
+    const input = document.querySelector('input[autocomplete="one-time-code"]') as HTMLInputElement;
+    if (!input) return;
+    const ac = new AbortController();
+    const form = input.closest("form");
+    if (form) {
+      form.addEventListener("submit", (e) => {
+        ac.abort();
+      });
+    }
+    // Request the OTP via get()
+    console.log('esperando OTP');
+    console.log('Input Seleccionado:',input);
+    input.focus()
+    
+    
+      navigator.credentials.get({
+        otp: { transport: ["sms"] },
+        signal: ac.signal} as any)
+      .then((otp: any) => {        
+        input.value = otp.code;
+        if (form) form.submit();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  // });
+}
+  }
+
+  ngAfterViewInit() {
+    console.log('Corremos');
+    
+    this.otpListener()
+  }
 }
