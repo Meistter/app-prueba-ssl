d42195566d01c050fe2e890e2f177d82ff1afb89
Meistter <sperez@bancomercantil.com>
2025-05-13 21:23:54 -0400

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,64 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import eruda from 'eruda';
-eruda.init()
+eruda.init();
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app-prueba-ssl';
 
-
-  otpListener(): void{
-    if ("OTPCredential" in window) {
+  otpListener(): void {
+    if ('OTPCredential' in window) {
+      console.log('Host:', window.location.host); // Host
+      console.log('HostName:', window.location.hostname); // Hostname
       console.log('Explorador valido');
-      
-  // window.addEventListener("DOMContentLoaded", (e) => {
-    const input = document.querySelector('input[autocomplete="one-time-code"]') as HTMLInputElement;
-    if (!input) return;
-    const ac = new AbortController();
-    const form = input.closest("form");
-    if (form) {
-      form.addEventListener("submit", (e) => {
-        ac.abort();
-      });
+
+      // window.addEventListener("DOMContentLoaded", (e) => {
+      const input = document.querySelector(
+        'input[autocomplete="one-time-code"]'
+      ) as HTMLInputElement;
+      console.log('input seleccionado', input);
+
+      if (!input) return;
+      const ac = new AbortController();
+      const form = input.closest('form');
+      if (form) {
+        form.addEventListener('submit', (e) => {
+          ac.abort();
+        });
+      }
+
+      console.log('Input Seleccionado:', input);
+      input.focus();
+      console.log('esperando OTP');
+
+      navigator.credentials
+        .get({
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        } as any)
+        .then((otp: any) => {
+          input.value = otp.code;
+          // if (form) form.submit();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      // });
+    } else {
+      console.log('Host:', window.location.host); // Host
+      console.log('HostName:', window.location.hostname); // Hostname
+      console.log('Explorador no compatible');
     }
-    // Request the OTP via get()
-    console.log('esperando OTP');
-    console.log('Input Seleccionado:',input);
-    input.focus()
-    
-    
-      navigator.credentials.get({
-        otp: { transport: ["sms"] },
-        signal: ac.signal} as any)
-      .then((otp: any) => {        
-        input.value = otp.code;
-        if (form) form.submit();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  // });
-}
   }
 
   ngAfterViewInit() {
     console.log('Corremos');
-    
-    this.otpListener()
+
+    this.otpListener();
   }
 }
